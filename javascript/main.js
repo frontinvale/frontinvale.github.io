69bfa458cfe60c7f1e04bb06ea229619c5e96d0b
Silvio Rodrigues <silviorodrigues7@gmail.com>
2019-05-08 22:51:41 -0300

    Update background transiction function

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,16 @@
 function randombg(){
-  var random = Math.floor(Math.random() * 11) + 0;
-  var bigSize = [
-    "url('images/bg-1.jpg')",
-    "url('images/bg-2.jpg')",
-    "url('images/bg-3.jpg')",
-    "url('images/bg-4.jpg')",
-    "url('images/bg-5.jpg')",
-    "url('images/bg-6.jpg')",
-    "url('images/bg-7.jpg')",
-    "url('images/bg-8.jpg')",
-    "url('images/bg-9.jpg')",
-    "url('images/bg-10.jpg')",
-    "url('images/bg-11.jpg')",
-  ];
-  document.getElementById("random").style.backgroundImage=bigSize[random];
+  var backgrounds = [
+    'url("images/bg-1.jpg")',
+    'url("images/bg-2.jpg")',
+    'url("images/bg-4.jpg")',
+    'url("images/bg-5.jpg")',
+    'url("images/bg-6.jpg")',
+    'url("images/bg-7.jpg")',
+    'url("images/bg-9.jpg")',
+    'url("images/bg-10.jpg")',
+    'url("images/bg-11.jpg")',
+  ],
+  random = Math.floor(Math.random() * backgrounds.length);
+
+  document.getElementById('random').style.backgroundImage = backgrounds[random];
 }
